3dbfbd9c642c4457cb75bb5e8a9e229698c1d855
Park Junwoo <ays99111@naver.com>
2025-02-12 01:42:04 +0900

    [feat] NasaVC URLSession(Delegate 방식)을 통한 통신 구현

--- URLSessionDelegate/View/NasaViewController.swift
+++ URLSessionDelegate/View/NasaViewController.swift
@@ -24,7 +24,13 @@ final class NasaViewController: UIViewController {
     }
     
     @objc private func buttonTapped() {
+        let urlRequest = URLRequest(url: Nasa.photo, timeoutInterval: 7)
         
+        let configure: URLSessionConfiguration = .default
+        
+        let request = URLSession(configuration: configure, delegate: self, delegateQueue: .main)
+        
+        request.dataTask(with: urlRequest).resume()
     }
     
     private func configureView() {
@@ -35,6 +41,7 @@ final class NasaViewController: UIViewController {
         self.progressLabel.text = "진행률 표시"
         self.progressLabel.textColor = UIColor.white
         self.progressLabel.backgroundColor = UIColor.black
+        self.button.setTitle("사진 불러오기", for: [])
         self.button.backgroundColor = UIColor.systemTeal
         self.button.addTarget(self, action: #selector(self.buttonTapped), for: .touchUpInside)
     }
@@ -62,4 +69,28 @@ final class NasaViewController: UIViewController {
         }
     }
 }
-
+extension NasaViewController: URLSessionDataDelegate {
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse) async -> URLSession.ResponseDisposition {
+        if let response = response as? HTTPURLResponse, (200..<300).contains(response.statusCode), let contentLength = response.value(forHTTPHeaderField: "Content-Length") {
+            
+            self.total = Double(contentLength) ?? 0
+            self.buffer = Data()
+            
+            return .allow
+        } else {
+            self.imageView.image = UIImage(systemName: "star.fill")
+            
+            return .cancel
+        }
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        self.buffer.append(data)
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: (any Error)?) {
+        if let error {
+            self.imageView.image = UIImage(systemName: "star.fill")
+        }
+    }
+}
